6f9e8c77e18e6900b39eae9995a8bf1ffb306b35
HrRosted <Hr.rosted@gmail.com>
2023-04-02 10:51:44 +0200

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uge_4_Fødselsdag
+{
+    internal class Person
+    {
+        private string navn;
+        private int alder;
+        private string adresse;
+        public string Navn
+        { get { return navn; } set { navn = value; } }
+        public int Alder
+        {
+            get { return alder; }
+            set
+            {
+                if (value >= 0)
+                {
+                    alder = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Alder skal være større end eller lig med 0");
+                }
+            }
+
+        }
+        public string Adresse { get { return adresse; } set { adresse = value; } }
+
+        public Person(string navn, int alder, string adresse)
+        {
+            Navn = navn;
+            Alder = alder;
+            Adresse = adresse;
+        }
+
+        public void Fødselsdag(int i)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                Alder++;
+            }
+        }
+        public override string ToString()
+        {
+            string s = $"{Navn} er nu {Alder} år og bor på adressen {Adresse}";
+            return s;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Uge_4_Fødselsdag
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string Navn = Ask("Navn: ");
+                int Alder = Int32.Parse(Ask("Alder: "));
+                string Adresse = Ask("Adresse: ");
+                int Fødselsdage = Int32.Parse(Ask("Hvor mange fødselsdage: "));
+
+                Person MigSelv = new Person(Navn, Alder, Adresse);
+
+                MigSelv.Fødselsdag(Fødselsdage);
+
+                Console.WriteLine(MigSelv.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("");
+                Console.WriteLine( "Fejl:");
+                Console.WriteLine( ex.Message);
+                Console.WriteLine("Programmet afsluttes!");
+            }
+        }
+        static string  Ask(string s)
+        {
+            String Answer = String.Empty;
+            while (String.IsNullOrWhiteSpace(Answer))
+            {
+                Console.Write(s);
+                Answer = Console.ReadLine();
+            }
+            return Answer;
+        }
+    }
+}
